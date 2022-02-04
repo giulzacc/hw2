8bc2111ec376b9d07b43ba9093bdb996e5784b5c
giulzacc <giuliana.zaccardelli@kellogg.northwestern.edu>
2022-02-04 19:32:50 +0000

    migrating data

--- kmdb.rb
+++ kmdb.rb
@@ -69,13 +69,18 @@
 # Delete existing data, so you'll start fresh each time this script is run.
 # Use `Model.destroy_all` code.
 # TODO!
+Movie.destroy_all
+Person.destroy_all
+Role.destroy_all
 
 # Generate models and tables, according to the domain model
 # TODO!
 
+
 # Insert data into your database that reflects the sample data shown above
 # Do not use hard-coded foreign key IDs.
 # TODO!
+
 
 # Prints a header for the movies output
 puts "Movies"
